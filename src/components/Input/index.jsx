115b7d9f2940cd840b9159acdef2de21b403ba76
lborgesc <lborgescaetano@gmail.com>
2022-06-15 17:28:30 -0300

    first commit

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import './styles.css'
+
+export class Input extends Component{ 
+    render ( ) {
+        return (
+            <input 
+                className="text-input"
+                onChange={this.props.onChange} 
+                value={this.props.value} 
+                type="search" 
+                placeholder="Type your search"
+            />
+        )
+    }
+}
